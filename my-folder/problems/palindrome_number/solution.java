ef1161b02a0ea4a417aa156f571187e5ea829c86
Yakov Byakov <105740597+tyasha@users.noreply.github.com>
2023-03-20 15:15:14 +0000

    Sync LeetCode submission - Palindrome Number (java)

--- my-folder/problems/palindrome_number/solution.java
+++ my-folder/problems/palindrome_number/solution.java
@@ -1,13 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String num = x + "";
-        char[] chars = num.toCharArray();
-        int j = 0;
-        for (int i = chars.length - 1; i >=0 ; i--) {
-            if (chars[j++] != chars[i]){
-                return false;
-            }
-        }
-        return true;
+        String number = x + "";
+        return  number.equals(new StringBuilder(number).reverse().toString());
     }
 }
